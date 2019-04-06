faf74de968cd50122e5043a35d3a5f51f78cceba
Anna Sabransky <anna.sabransky@gmail.com>
2019-04-06 17:31:52 +0200

    Arrays practice

--- week-02/day-01/src/test.java
+++ week-02/day-01/src/test.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class test {
@@ -80,6 +81,82 @@ public class test {
             }
             System.out.println('|');
         }
+
+        System.out.print("\n");
+
+
+
+        //copies out the first 5 indexes out numberArrays
+        int[] numberCopy = Arrays.copyOf(numberArray, 5);
+
+        for (int row: numberCopy) {
+            System.out.print(row);
+        }
+        System.out.print("\n");
+
+        //copies a range
+
+        int[] number2Copy = Arrays.copyOfRange(numberArray, 3, 6);
+
+        for (int row: number2Copy) {
+            System.out.print(row);
+        }
+        System.out.print("\n");
+
+        System.out.println(Arrays.toString(number2Copy));
+
+        //----------------------------------
+
+        //assigns the numbr 2 to all indexes
+
+
+        int[] moreNumbers = new int[100];
+
+        Arrays.fill(moreNumbers, 2);
+
+        //fill in multidimensonal
+
+        char[][] boardGame = new char [10][10];
+        for(char[] row : boardGame) {
+            Arrays.fill(row, '*');
+        }
+
+        for(char[] rows : boardGame) {
+            for (char column : rows) {
+                System.out.print("|" + column + " " );
+            }
+            System.out.println('|');
+        }
+
+        System.out.print("\n");
+
+        //--------------------------------------
+        // sort an array
+
+        int[] numToSort = new int[10];
+
+        //assigns random értékek
+
+        for(int i = 0; i < 10; i++) {
+            numToSort[i] = (int) (Math.random() * 100);
+        }
+
+        //sort them in ascending order
+        Arrays.sort(numToSort);
+
+        System.out.println(Arrays.toString(numToSort));
+
+        //binary search: returns the index for the search value, otherwise returns A negative number
+        //I want to find the value of 50 if it exists
+
+
+        int whereIs50 = Arrays.binarySearch(numToSort, 50);
+        System.out.println(whereIs50);
+
+
+
+
+
 
 
 
