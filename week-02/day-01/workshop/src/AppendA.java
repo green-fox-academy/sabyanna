3e2b95273e0e9169d318d771867d77360172d62e
Anna Sabransky <anna.sabransky@gmail.com>
2019-04-08 12:51:58 +0200

    workshop2

---
+++ week-02/day-01/workshop/src/AppendA.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class AppendA {
+    public static void main(String[] args) {
+
+        String [] animals = {"koal", "pand", "zebr"};
+
+        for(int i = 0; i < 3; i++) {
+            animals[i] = animals[i].concat("a");
+        }
+
+        System.out.println(Arrays.toString(animals));
+
+        // - Create an array variable named `animals`
+        //   with the following content: `["koal", "pand", "zebr"]`
+        // - Add all elements an `"a"` at the end
+    }
+}
