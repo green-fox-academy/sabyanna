3938f6cfe29d7d11267a14ca8bc47f7fb3a7c177
Anna Sabransky <anna.sabransky@gmail.com>
2019-04-13 23:45:10 +0200

    extra triangles2

--- week-02/day-05/extra/src/TwoTriangles.java
+++ week-02/day-05/extra/src/TwoTriangles.java
@@ -24,7 +24,7 @@ public class TwoTriangles {
         int yyp[] = {0, 0, HEIGHT};
         g.drawPolygon(xxp, yyp, 3);
 
-        triangles(g, 0, 1, 1, (int)(wid), side);
+        triangles(g, 0, 1, 1, (int)(wid), 0, side/2, hei);
     }
 
     public static void setBackGround(Graphics a) {
@@ -32,15 +32,16 @@ public class TwoTriangles {
         a.fillRect(0, 0, (int)(WIDTH), (int)(WIDTH));
     }
 
-    public static void triangles(Graphics g, int i, double magic, double MAGIC, int x1, double Side) {
-        int xp[] = {(int)(x1 * magic), (int)(x1 - Side/2 * magic), (int)(x1+Side/2 * magic)};
-        int yp[] = {0, (int)(hei * MAGIC), (int)(hei * MAGIC)};
+    public static void triangles(Graphics g, int i, double magic, double MAGIC, int x1, int y1, double Side, double height) {
+        int xp[] = {(int)(x1*magic), (int)((x1 - Side)*magic), (int)((x1+Side)*magic)};
+        int yp[] = {(int)(y1), (int)(y1 +height * MAGIC), (int)(y1 + height * MAGIC)};
         g.drawPolygon(xp,yp,3);
         i++;
 
         if (i < 6) {
-            triangles(g, i, magic/2, MAGIC/2, x1,Side /2);
-            triangles(g, i, magic*3/2, MAGIC/2, x1,Side /2);
+            triangles(g, i, magic, MAGIC/2, (int)(x1+Side), y1, Side/2, height);
+            triangles(g, i, magic/2, MAGIC/2, x1, y1, Side, height);
+            triangles(g, i, magic, MAGIC, x1, (int)(y1+height), Side/2, height/2);
 
         }
     }
