39722b6036cd20ed79fd2beef453b7d6fad5cf57
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-14 11:29:49 +0200

    squared bigger than 20 done

--- week-06/day-02/src/Squared20.java
+++ week-06/day-02/src/Squared20.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Squared20 {
   public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
 
+    numbers.stream()
+            .filter(x -> x * x > 20)
+            .forEach(x -> System.out.println(x));
   }
 }
