5980aa0964c1b58b41d893b941a8318b58aad4b5
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-15 15:02:40 +0200

    sayhi prints out all greetings

--- week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/GreetCounter.java
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/GreetCounter.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -10,10 +11,10 @@ public class GreetCounter {
   Greeting myGreet = new Greeting( "Hi ");
 
   @RequestMapping("/web/greetCount")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam(required = true) String name) {
+
+    model.addAttribute("name", name);
     model.addAttribute("id", myGreet.getId().getAndAdd(1));
     return "greetCounter";
   }
-
 }
