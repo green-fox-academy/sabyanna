94a78109b85afa5f1fffdc7661372d0cf4af8bad
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-15 16:21:37 +0200

    hier colors change

--- week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Hier.java
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Hier.java
@@ -8,20 +8,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Arrays;
 
 @Controller
-public class MultiHellos {
+public class Hier {
+  Greeting myGreet = new Greeting( "Hi ");
   String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
           "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
           "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
           "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
-
-  @RequestMapping("/web/sayhi")
-  public String greeting(Model model) {
-
-    model.addAttribute("greets", hellos);
-    model.addAttribute("name", "Anna");
-    return "multiHellos";
+  @RequestMapping("/web/hier")
+  public String greeting(Model model, @RequestParam(required = true) String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("greet", hellos[(int)myGreet.getId().getAndAdd(1)]);
+    model.addAttribute("color", "color: rgb(" + Math.random()*255 + ", " + Math.random()*255 + ", " + Math.random()*255 + ")");
+    return "hier";
   }
 }
 //rgb(0, 0, 224)
 //<p style="color: red" th:text="${name} + '! ,'"/>
+//hellos[(int)myGreet.getId().getAndAdd(1)]
