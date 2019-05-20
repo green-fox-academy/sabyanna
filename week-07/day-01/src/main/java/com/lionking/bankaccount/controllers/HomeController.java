6f43ee36e46884b52b60b49fa2da2a1e5ca188ee
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-20 14:55:28 +0200

    feat(table)

--- week-07/day-01/src/main/java/com/lionking/bankaccount/controllers/HomeController.java
+++ week-07/day-01/src/main/java/com/lionking/bankaccount/controllers/HomeController.java
@@ -5,15 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.constraints.Max;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HomeController {
 
-  BankAccount account;
+  BankAccount simba = new BankAccount("Simba", 2000, "lion");
+
 
   @RequestMapping("/show")
   public String showAccount(Model model) {
-    account = new BankAccount("Simba", 2000, "lion");
-    model.addAttribute("account", account);
+    model.addAttribute("account", simba);
     return "index";
   }
 
@@ -21,6 +25,18 @@ public class HomeController {
   public String weirdEnd(Model model) {
     model.addAttribute("String", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
     return "stringer";
+  }
+
+@RequestMapping("/table")
+  public String inTable(Model model) {
+    List<BankAccount> accounts = new ArrayList<>();
+    accounts.add(simba);
+    accounts.add(new BankAccount("Zazu", 500, "bird"));
+    accounts.add(new BankAccount("Nala", 1000, "lion"));
+    accounts.add(new BankAccount("Ed", 2000, "hyena"));
+    accounts.add(new BankAccount("Scar", 5000, "lion"));
+    model.addAttribute("accounts", accounts);
+    return "table";
   }
 
 
