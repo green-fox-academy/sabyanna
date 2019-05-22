61b1a4ddad0e97c6adb0730b1893aaa2a5bc6890
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-22 12:58:13 +0200

    feat(templates and UtilityService created)

--- week-07/day-03/src/main/java/com/greenfox/hellobeanproject/HellobeanprojectApplication.java
+++ week-07/day-03/src/main/java/com/greenfox/hellobeanproject/HellobeanprojectApplication.java
@@ -1,13 +1,30 @@
 package com.greenfox.hellobeanproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellobeanprojectApplication {
+public class HellobeanprojectApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(HellobeanprojectApplication.class, args);
   }
 
+  private Printer printer;
+
+  HellobeanprojectApplication(Printer printer) {
+    this.printer = printer;
+  }
+
+  @Autowired
+  private MyColor myColor;
+
+
+  @Override
+  public void run(String... args) throws Exception {
+    //printer.log("hello");
+    myColor.printColor();
+  }
 }
