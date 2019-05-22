0d6cb4999028e1e380db68df79d12c611acf0a33
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-22 16:29:15 +0200

    feat(GreenFoxController):counter

--- week-07/day-03-02/src/main/java/com/greenfox/hellos/Controllers/GreenFoxController.java
+++ week-07/day-03-02/src/main/java/com/greenfox/hellos/Controllers/GreenFoxController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreenFoxController {
@@ -17,7 +18,7 @@ public class GreenFoxController {
 
   @GetMapping("/gfa")
   public String Colored(Model model) {
-
+    model.addAttribute("counter", "Currently we have " + studentService.studentCounter() + " users");
     return "gfa";
   }
 
@@ -26,4 +27,19 @@ public class GreenFoxController {
     model.addAttribute("students", studentService.findAll());
     return "list";
   }
+
+  @GetMapping("/gfa/add")
+  public String Add(Model model) {
+    return "add";
+  }
+
+  @PostMapping("/gfa/save")
+  public String handlingForm(Model model,String name) {
+    studentService.save(name);
+    model.addAttribute("students", studentService.findAll());
+    return "list";
+  }
+
+
+
 }
