e8e3e00179de44d5ddb463daaa9cd6f70832e471
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-22 15:45:16 +0200

    feat(GreenFoxController):list

--- week-07/day-03-02/src/main/java/com/greenfox/hellos/Controllers/GreenFoxController.java
+++ week-07/day-03-02/src/main/java/com/greenfox/hellos/Controllers/GreenFoxController.java
@@ -1,14 +1,29 @@
 package com.greenfox.hellos.Controllers;
 
+import com.greenfox.hellos.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GreenFoxController {
-  @GetMapping("/useful/colored")
+
+  @Autowired
+  @Qualifier("StService")
+  StudentService studentService;
+
+
+  @GetMapping("/gfa")
   public String Colored(Model model) {
-    model.addAttribute("color", "red");
-    return "colored";
+
+    return "gfa";
+  }
+
+  @GetMapping("/gfa/list")
+  public String List(Model model) {
+    model.addAttribute("students", studentService.findAll());
+    return "list";
   }
 }
