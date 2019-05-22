924aad563e330386dc9a9dce5e9e146def2270cd
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-22 11:41:14 +0200

    feat(Printer class created)

---
+++ week-07/day-03/src/main/java/com/greenfox/hellobeanproject/HellobeanprojectApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.hellobeanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanprojectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanprojectApplication.class, args);
+  }
+
+}
